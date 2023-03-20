abad93e1539e45e19969996e135b67f030386be4
Fedekh <federicocet@gmail.com>
2023-03-20 23:42:54 +0100

    i try to add all BONUS 2, in the same folder, and avoi to let click if user loses

--- js/script.js
+++ js/script.js
@@ -6,7 +6,8 @@
 // Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato su una cella che non era una bomba.
 
 // **BONUS:**
-// 1 - L'utente indica un livello di difficoltà in base al quale viene generata una griglia di gioco quadrata, in cui ogni cella contiene un numero tra quelli compresi in un range:
+// 1 - L'utente indica un livello di difficoltà in base al quale viene generata una griglia di gioco quadrata,
+// in cui ogni cella contiene un numero tra quelli compresi in un range:
 // con difficoltà 1 => tra 1 e 100
 // con difficoltà 2 => tra 1 e 81
 // con difficoltà 3 => tra 1 e 49
@@ -48,34 +49,25 @@ const grid = document.querySelector(".grid") // griglia le cui caselle dipendera
 let gridGenerated = false; // mi dice se la grid è stata generata gia o meno
 let choise = document.getElementById("select"); // variabile legata al menu a tendina della difficolta
 const numberBombs = 16;
-let bombs=[];
+let bombs = []; // array vuoto che conterra i famosi 16 numeri randomici di bombe
+let gameOver = false; //la uso come flag per vedere se c'è il gameover o meno 
+
 
 
 
 // CLICK EVENTO BOTTONE PLAY
 
-btn.addEventListener('click', function() {
+btn.addEventListener('click', function () {
     const mode = choise.value; // modalita scelta dal menu a tendina
     const numberOfSquare = regulationGrid(mode); //richiamo funzione per dare il numero di elementi della grid
-    const lastNumberGrid = generateGrid(numberOfSquare); 
-    bombs = generateBomb (16, regulationGrid(mode)); // genero array di bombe
+    const lastNumberGrid = generateGrid(numberOfSquare);
+    bombs = generateBomb(16, regulationGrid(mode)); // genero array di bombe
     console.log(bombs);
 });
 
 
 // **#############################################################################
 // ! FUNZIONI
-
-//
- /// Funzione che mi genera le celle cliccabili massime senza incorrere a bombe
- // @param {number} generateBombs 
- // @param {number} leftCells 
- // @returns  {number}
- ///
-// function maxClick (generateBombs, leftCells){
-//     let clickMax = generateBomb() - choise.value ;
-//     return clickMax;
-// }
 
 
 /**
@@ -84,17 +76,17 @@ btn.addEventListener('click', function() {
  * @param {number} maxNumber (puo essere o 100 o 81 o 49)
  * @returns {array} di 16 numeri casuali
  */
-function generateBomb (numbersQuantity, maxNumber){
+function generateBomb(numbersQuantity, maxNumber) {
     // creare array vuoto
     const numbers = [];
     // Finche la lunghezza array è < di numbersQuantity (16)
-    while (numbers.length < numbersQuantity){
+    while (numbers.length < numbersQuantity) {
         //     genero un numero random nel Range (100-81-49), creando all interno di questa funzione , un ulteriore funzione, dando gli estremi del range
-        const rndNumber = getRndNumber (1 , maxNumber);
+        const rndNumber = getRndNumber(1, maxNumber);
         //     se il numero non è gia presente nell array lo Push , altrimenti continuo a generare
-            if (!numbers.includes (rndNumber)){
-                numbers.push (rndNumber);
-            }
+        if (!numbers.includes(rndNumber)) {
+            numbers.push(rndNumber);
+        }
     }
     return numbers;
 }
@@ -106,8 +98,8 @@ function generateBomb (numbersQuantity, maxNumber){
  * @param {number} max
  * @returns {number} il numero randomico mi ritorna
  */
-function getRndNumber (min = 1, max = 100){
-    return Math.floor(Math.random() * (max - min) ) + min;    
+function getRndNumber(min = 1, max = 100) {
+    return Math.floor(Math.random() * (max - min)) + min;
 }
 // console.log(getRndNumber(1, 49));
 
@@ -129,39 +121,48 @@ function generateGrid(numberOfSquares) {
         currentElem.addEventListener("click", itemClick);// dopo la creazione griglia posso aggiungere la chiamata alla funzione tale per cui si colora lo sfondo di blu
         grid.appendChild(currentElem); //aggiungo l'elemento alla griglia       
     }
-    
+
 }
 
 
 // CREO funzione che in base alla scelta dell'utente mi cambia anche il numero degli elementi della grid
-function regulationGrid (modeUser) {
+function regulationGrid(modeUser) {
     let numberSquare = 0;
-    if (modeUser === "easy"){
-        numberSquare = 100 ;
+    if (modeUser === "easy") {
+        numberSquare = 100;
     } else if (modeUser === "medium") {
-        numberSquare = 81 ;
-    } else if (modeUser === "hard"){
-        numberSquare = 49 ;
+        numberSquare = 81;
+    } else if (modeUser === "hard") {
+        numberSquare = 49;
     }
 
     return numberSquare;
 }
 
+function gameOverAlert() { //funzione che allerta che hai perso
+    return alert("Hai perso!");
+  }
 
 // Modifico la funzione in modo da tener conto della cella cliccata
-function itemClick() {    
-    this.classList.add("click");    
-    clickedNumber = parseInt(this.innerText); 
+function itemClick() {
+    this.classList.add("click"); //aggiungo la classe click ovvvero sfondo blu
+    clickedNumber = parseInt(this.innerText); //mostro l'intero equivalente a cio che è scritto in cella
     console.log(clickedNumber, typeof clickedNumber);
-    console.log("HAI CLICCATO" , this.innerText); 
-    console.log(bombs);
-    if (bombs.includes(clickedNumber)){
-        console.log("hai perso", clickedNumber);
-        
+    console.log("HAI CLICCATO", this.innerText);
+    
+    if (bombs.includes(clickedNumber)) {
+        console.log("HAI PERSO", clickedNumber);
+        this.classList.add("bomb");
+        gameOverAlert();// funzione che esplica un alert in caso di sconfitta
+        // this.innerHTML = ""; // reset game
+        const items = document.querySelectorAll(".item");
+        items.removeEventListener("click", itemClick);      
+       
     } else {
         console.log("puoi continuare");
+        return clickedNumber;
     }
-    return clickedNumber;    
+
 }
 
 
